aea9a42c6cb2fe8ef9e66107db22faceed097eb5
piyush-vaviya <piyush.vaviya27@gmail.com>
2023-08-21 23:03:50 +0530

    feat: path intellisense setup

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import jsconfigPaths from "vite-jsconfig-paths";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), jsconfigPaths],
   server: {
     port: 5000,
+  },
+  resolve: {
+    alias: {
+      "@components": ["/src/components"],
+      "@": ["/src"],
+    },
   },
 });
